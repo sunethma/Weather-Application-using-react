872aa69045a9ff462f4dbab9ccedb1387bac885d
Sunethma Sandakini <103886731+sunethma@users.noreply.github.com>
2023-06-29 01:29:06 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import '/node_modules/bootstrap/dist/css/bootstrap.min.css'
-//import 'bootstrap/dist/js/bootstrap.min.js';
 import './Components/WeatherInformation.css';
 
 
@@ -12,10 +11,3 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <App />
   </React.StrictMode>,
 
-)
-/*ReactDOM.createRoot(document.getElementById('container overflow-hidden text-center') as HTMLElement).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-
-)*/
